9e8d65e2faca258471a8bd4a20d93f013ad1d29f
Ovidiu Maghetiu <ovidiumght@gmail.com>
2023-05-08 22:57:48 +0300

    Added more docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>WPCodeBox 2.0 Documentation</span>,
+  logo: <span>WPCodeBox 2 Documentation</span>,
   feedback:  { content: null},
   editLink: { text: null},
   footer: {
