7d48e0a3614595017bb8c511940c1451901b4571
Ovidiu Maghetiu <ovidiumght@gmail.com>
2023-05-19 21:56:35 +0300

    Remoevd DS Strore

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,28 +1,32 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig} from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>WPCodeBox 2 Documentation</span>,
-  feedback:  { content: null},
-  editLink: { text: null},
-  footer: {
-    text: 'WPCodeBox Documentation',
-  },
-  head: (
-      <>
-        <meta property="og:title" content="WPCodeBox 2 Documentation" />
-        <meta property="og:description" content="WPCodeBox 2 Documentation" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.svg"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.svg"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.svg"/>
+    logo: <span>WPCodeBox 2 Documentation</span>,
+    project: {
+        link: 'https://github.com/wpcodebox/documentation'
+    },
+    docsRepositoryBase: 'https://github.com/wpcodebox/documentation/blob/main/',
+    feedback: {content: 'Feedback'},
+    editLink: {text: 'Edit This Page'},
+    footer: {
+        text: 'WPCodeBox Documentation',
+    },
+    head: (
+        <>
+            <meta property="og:title" content="WPCodeBox 2 Documentation"/>
+            <meta property="og:description" content="WPCodeBox 2 Documentation"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon.svg"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon.svg"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon.svg"/>
 
-          </>
-  ),
-  useNextSeoProps() {
-    return {
-      titleTemplate: '%s – WPCodeBox Documentation',
+        </>
+    ),
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – WPCodeBox Documentation',
+        }
     }
-  }
 }
 
 export default config
